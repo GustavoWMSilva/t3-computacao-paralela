95300209ead7cd82765d0712923bb0564fed8209
Henrique Feijó <henrique.paim01@edu.pucrs.br>
2026-06-16 18:29:57 -0300

    Add files via upload

---
+++ ladEscalonamentoForte.sh
@@ -0,0 +1,6 @@
+srun -N 2 -n 2 ./mult_mpi 2048
+srun -N 2 -n 3 ./mult_mpi 2048
+srun -N 2 -n 5 ./mult_mpi 2048
+srun -N 2 -n 9 ./mult_mpi 2048
+srun -N 2 -n 17 ./mult_mpi 2048
+srun -N 3 -n 33 ./mult_mpi 2048
